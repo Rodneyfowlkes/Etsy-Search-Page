2afae8b84ee042a5c865a6d21fd7a8870a6febcb
Rodney Fowlkes <Fowlkesrodney@gmail.com>
2016-06-24 12:45:35 -0400

    Began working on the basis of my template literal and worked through the nested data retrieval function

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
 import $ from 'jquery';
+// I need the item image
+// The item "title" 
+// Item "url" 
+// The items maker "Shop"--"shop_name"
+// The items price and currency "price" "currency_code"
+
+// items.results.forEach(function(x){
+
+// console.log(x.price)
+// console.log(x.currency_code)
+// console.log(x.title)
+// console.log(x.url)
+// console.log(x.Shop.shop_name)
+// console.log(x.Shop.url)
+
+
+
+// });
